d4660ab851f90795da1874f837f51bc676202316
agamenon <agamenon_junior1@hotmail.com>
2025-12-12 18:00:38 -0300

    feat: Atualizar produto

--- src/main/java/io/github/agamenonjunior/produtosapi/controller/ProdutoController.java
+++ src/main/java/io/github/agamenonjunior/produtosapi/controller/ProdutoController.java
@@ -51,4 +51,10 @@ public class ProdutoController {
         produtoRepository.deleteById(id);
     }
 
+    @PutMapping("{id}")
+    public void atualizar(@PathVariable("id") String id, @RequestBody Produto produto){
+            produto.setId(id);
+            produtoRepository.save(produto);
+    }
+
 }
